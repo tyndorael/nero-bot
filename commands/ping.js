e64157127daae242d1171d7e61d2e7b31f047208
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2020-09-03 16:07:04 -0400

    change to python

--- commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Ping!',
-  cooldown: 5, // seconds
-  execute(message) {
-    return message.channel.send('Pong.');
-  },
-};
--- commands/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'server',
-  description: 'Server info',
-  execute(message) {
-    message.channel.send(
-      `Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`,
-    );
-  },
-};
